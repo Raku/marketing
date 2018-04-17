dbe669e1b27c0edada7c9210ac86cf80be16ef2b
Zoffix Znet <cpan@zoffix.com>
2018-04-16 20:31:31 -0400

    Implement thumblerizer

---
+++ thumbnailarize.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use 5.026;
+use strict;
+use warnings;
+use Mojo::File qw/path/;
+
+@ARGV or die "Usage: $0 path/to/file.pdf";
+my $pdf = path shift;
+
+my $pic_dir = path($pdf->dirname)->child('thumbs')->make_path;
+system 'convert', qw/-colorspace RGB -background white -alpha remove/, $pdf,
+    qw/-antialias -resize 178>/, $pic_dir->child($pdf->basename . '.jpg');
+
