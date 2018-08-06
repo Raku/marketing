5c5562f06ea4f401a1ec694b1f64368d5c0a6de6
Zoffix Znet <cpan@zoffix.com>
2018-08-05 21:20:52 -0400

    Fix up app path in cronscripts

--- web/start-marketing.perl6.org.sh
+++ web/start-marketing.perl6.org.sh
@@ -3,14 +3,14 @@
 set -e -x
 echo 'Starting marketing.perl6.org update'
 date
-cd ~/marketing/
+cd ~/marketing/web/
 
 git fetch
 before=$(git rev-parse HEAD)
 git checkout origin/master
 after=$(git rev-parse HEAD)
-cp update-marketing.perl6.org.sh ../
-cp start-marketing.perl6.org.sh  ../
+cp update-marketing.perl6.org.sh ../../
+cp start-marketing.perl6.org.sh  ../../
 
 if [ "$before" != "$after" ]
 then
